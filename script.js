736ff40d32dae142901bff3a2a35ac6c6c16c27c
cayleb247 <156046197+cayleb247@users.noreply.github.com>
2024-08-12 01:01:11 -0400

    Allow users to change squares per side
    Made a button for users to adjust the size of the grid.
    Made function to clear the board when the button is clicked.
    Took the user input as input for grid creation.

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 const container = document.querySelector(".container");
 
+const button = document.querySelector("button");
+
+function clearGrid() {
+    let e = document.querySelector(".container");
+    e.innerHTML = "";
+}
+
 // Create grid
 for (let i = 0; i < 16; i++) {
     for (let i = 0; i < 16; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener("mouseenter", (e) => {
+            e.target.style.background = 'black';
+        })
         container.appendChild(square);
     }
 
 }
+
+button.addEventListener("click", () => {
+    let perSide = prompt("How many squares to a side? (Values 100 or less)");
+    clearGrid();
+    // Create grid
+    for (let i = 0; i < perSide; i++) {
+        for (let i = 0; i < perSide; i++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.height = Math.floor((640 / perSide) - 2) + "px";
+            square.style.width = Math.floor((640 / perSide) - 2) + "px";
+            square.addEventListener("mouseenter", (e) => {
+                e.target.style.background = 'black';
+            })
+            container.appendChild(square);
+        }
+
+}
+})
+
+
+
