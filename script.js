d936bd25f1d412fd5acb8f385a95845f2135cac4
cayleb247 <156046197+cayleb247@users.noreply.github.com>
2024-08-13 00:50:07 -0400

    Fix grid generation and add random color drawing

--- script.js
+++ script.js
@@ -13,31 +13,37 @@ for (let i = 0; i < 16; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.addEventListener("mouseenter", (e) => {
-            e.target.style.background = 'black';
+            e.target.style.background = randomColor();
         })
         container.appendChild(square);
     }
 
 }
 
+const randomColor = () => {
+    return `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}`;
+}
+
 button.addEventListener("click", () => {
     let perSide = prompt("How many squares to a side? (Values 100 or less)");
-    clearGrid();
-    // Create grid
-    for (let i = 0; i < perSide; i++) {
-        for (let i = 0; i < perSide; i++) {
-            const square = document.createElement("div");
-            square.classList.add("square");
-            square.style.height = Math.floor((640 / perSide) - 2) + "px";
-            square.style.width = Math.floor((640 / perSide) - 2) + "px";
-            square.addEventListener("mouseenter", (e) => {
-                e.target.style.background = 'black';
-            })
-            container.appendChild(square);
-        }
 
+    if (perSide <= 100) {
+        clearGrid();
+            // Create grid
+        for (let i = 0; i < perSide; i++) {
+            for (let i = 0; i < perSide; i++) {
+                const square = document.createElement("div");
+                square.classList.add("square");
+                square.style.height = (800 / perSide) + "px";
+                square.style.width = (800 / perSide) + "px";
+                square.addEventListener("mouseenter", (e) => {
+                    e.target.style.background = randomColor();
+                })
+                container.appendChild(square);
+            }
+    }
+    
+} else {
+    alert("Invalid input :(");
 }
 })
-
-
-
